c8a2d85999c90faef4c8007f21b8b1e905fff090
VictorTubei <vktubei@gmail.com>
2025-01-16 18:48:06 +0300

    completed filter array lab

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solution here
+function findMatching(drivers, name) {
+    return drivers.filter(driver => driver.toLowerCase() === name.toLowerCase());
+}
+const drivers = ["Abel", "Mutua", "Njugush", "Munyambu"];
+console.log(findMatching(drivers, "mutua"));
+
+function fuzzyMatch(drivers, letters) {
+    return drivers.filter(driver => driver.toLowerCase().startsWith(letters.toLowerCase()));
+}
+
+console.log(fuzzyMatch(drivers, "Mu"));
+
+function matchName(drivers, name) {
+    return drivers.filter(driver => driver.name.toLowerCase() === name.toLowerCase());
+}
+
+const driverObjects = [
+    { name: "Abel", hometown: "Macha" },
+    { name: "Njugush", hometown: "Kiambu" },
+    { name: "Wakavinye", hometown: "Kitengela" },
+];
+console.log(matchName(driverObjects, "abel"));
